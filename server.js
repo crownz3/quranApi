59b6dd9206a3f96df77d72f15d10fe09aa0d9d6e
Ibrahim <112082276+crownz3@users.noreply.github.com>
2025-05-28 15:32:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.get('/api/wordbyword/:token/:surah/:ayah', async (req, res) => {
 
     const response = await axios.get(
       // `https://apis-prelive.quran.foundation/content/api/v4/verses/by_key/${verseKey}?language=ta&words=true&translations=158`,
-      `https://apis.quran.foundation/content/api/v4/verses/by_key/${verseKey}?words=true&translations=133&word_translation_language=ta&fields=text_uthmani&word_fields=verse_key,text_uthmani&translation_fields=resource_name,language_id&mushaf=2`,
+      `https://apis.quran.foundation/content/api/qdc/verses/by_key/${verseKey}?words=true&translations=133&word_translation_language=ta&fields=text_uthmani&word_fields=verse_key,text_uthmani&translation_fields=resource_name,language_id&mushaf=2`,
       {
         headers: {
           'x-auth-token': token.trim(),
